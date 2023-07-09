f6c7f63c9281c9b7c552df7c576c1102420d575b
wbhaao <a31620504@gmail.com>
2023-07-09 12:29:29 +0900

    express 값 받는법

--- tryhard/src/App.js
+++ tryhard/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+  const [backendData, setBackendData] = useState([{}])
+  useEffect(() =>{
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        (typeof backendData.user === undefined) ? 
+        (<p>Loading</p>) : 
+        (<p>{backendData.user}</p>)
+      }
     </div>
   );
 }
